980a7f9a5771923f0c281d97ca0e5a5dc11d9baf
Shyam-Prag <pragshyam11@gmail.com>
2024-08-14 07:11:30 +0000

    init

---
+++ bin/btc-discord-bot-cdk.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { BtcDiscordBotCdkStack } from '../lib/btc-discord-bot-cdk-stack';
+
+const app = new cdk.App();
+new BtcDiscordBotCdkStack(app, 'BtcDiscordBotCdkStack', {
+
+});
---
+++ test/btc-discord-bot-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BtcDiscordBotCdk from '../lib/btc-discord-bot-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/btc-discord-bot-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BtcDiscordBotCdk.BtcDiscordBotCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
