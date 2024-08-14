6a89474e441f5f0202d1cff4038da93a1142d8c3
Shyam-Prag <pragshyam11@gmail.com>
2024-08-14 09:21:48 +0000

    migration done

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,5 +1,89 @@
+import os 
+import boto3
+import botocore 
+from PIL import Image, ImageDraw, ImageFont
+import requests
+from discord_webhook import DiscordWebhook, DiscordEmbed
+from discordwebhook import Discord
+import json
+# patch all to send traces to X-RAY - https://docs.aws.amazon.com/xray/latest/devguide/xray-sdk-python-patching.html
+# Trying to send traces for outbound HTTP requests 
 
+
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
+
+patch_all()
+
+### End of patching for X-RAY instrumentation
+
+DISCORD_WEBHOOK_URL= os.environ["DISCORD_WEBHOOK_URL"]
+API_KEY = os.environ["API_KEY"]
+BTC_URL = os.environ["BTC_URL"]
+webhook = DiscordWebhook(url=f"{DISCORD_WEBHOOK_URL}")
+discord = Discord(url=DISCORD_WEBHOOK_URL)
 
 def lambda_handler(event, context):
+    # LETS GET THE PRICE OF BTC
+    url = BTC_URL
     
-    return "hello test"
+    querystring = {"referenceCurrencyUuid":"yhjMzLPhuIDl"}
+    
+    headers = {
+        "X-RapidAPI-Key": f"{API_KEY}",
+        "X-RapidAPI-Host": "coinranking1.p.rapidapi.com"
+    }
+    
+    response = requests.get(url, headers=headers, params=querystring)
+    
+    # Convert the response object to a Python dictionary
+    response_dict = json.loads(response.text)
+    
+    # Extract the price value
+    BTC_Price = response_dict["data"]["price"]
+    
+    floated_value = float(BTC_Price)
+    
+    BTC_Price_rounded = round(floated_value, 2)
+    
+    BTC_message = f'The price of Bitcoin is: ${BTC_Price_rounded}'
+    
+    ##### CREATES IMAGE ############################################################
+    print(os.listdir('/opt'))
+    print(os.listdir('/opt/python'))
+    
+    
+    new = Image.new('RGB', (200,100), color=(255, 153, 0))
+    
+    d = ImageDraw.Draw(new)
+    
+    #font = 'arial'
+    font = ImageFont.truetype(r'/opt/python/verdanaz.ttf', 25)  
+    
+    d.text((65,5), "BTC", font = font, fill=(0,0,0), align = "left")
+    d.text((80,30), "=", font = font, fill=(0,0,0), align = "left")
+    d.text((30,60), f"${BTC_Price_rounded}", font = font, fill=(0,0,0), align = "left")
+    
+    
+    new.save('/tmp/new_bitcoin_img.png')
+    
+    print('tmp dir')
+    print(os.listdir('/tmp'))
+    
+    #################################################################
+    # LETS SEND MESSAGE TO DISCORD VIA WEBHOOK
+    webhook = DiscordWebhook(url=f"{DISCORD_WEBHOOK_URL}")
+
+    with open("/tmp/new_bitcoin_img.png", "rb") as f:
+        webhook.add_file(file=f.read(), filename="new_bitcoin_img.png")
+    
+    embed = DiscordEmbed(title="BTC UPDATE", description= f"The price of BTC is ${BTC_Price_rounded}", color="03b2f8")
+    
+    embed.set_thumbnail(url="attachment://new_bitcoin_img.png")
+    
+    webhook.add_embed(embed)
+    response = webhook.execute()
+    
+    
+    return "hello"
+    
--- lib/btc-discord-bot-cdk-stack.ts
+++ lib/btc-discord-bot-cdk-stack.ts
@@ -1,16 +1,51 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import {LAMBDA_ROLE, LAMBDA_LAYER_ARN, BTC_URL, API_KEY, DISCORD_WEBHOOK_URL, FONT_LAYER} from '../assets/application';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Duration } from 'aws-cdk-lib';
+
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';   
 
 export class BtcDiscordBotCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'BtcDiscordBotCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const btcFunction = new lambda.Function(this, 'btcFunction', {
+      runtime: lambda.Runtime.PYTHON_3_12, 
+      code: lambda.Code.fromAsset('lambda'), 
+      handler: 'lambda_function.lambda_handler',
+      timeout: Duration.seconds(900),
+      memorySize: 512,
+      layers: [
+        lambda.LayerVersion.fromLayerVersionArn(this, 'BtcLambdaLayer', LAMBDA_LAYER_ARN),
+        lambda.LayerVersion.fromLayerVersionArn(this, 'verdanazFontLambdaLayer', FONT_LAYER),
+      ],
+      environment: {
+        API_KEY: API_KEY,
+        BTC_URL: BTC_URL,
+        DISCORD_WEBHOOK_URL: DISCORD_WEBHOOK_URL
+      },
+    });
+    
+    
+    const btcFunctionRole = new iam.Role(this, 'btcFunctionRole', {
+      roleName: `${LAMBDA_ROLE}`,
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole')],
+    });
+    
+    // add eventbridge cron job 
+    const cronRule = new events.Rule(this, 'CronRule', {
+      schedule: events.Schedule.expression('rate(4 hours)')
+    });
+    
+    cronRule.addTarget(new targets.LambdaFunction(btcFunction))
+    
+    
+    
+    
+    
   }
 }
